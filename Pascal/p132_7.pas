58fa67e118adea74d9ac827a63097040cac3152d
MinedroidScr <mineall@mail.ru>
2018-02-10 18:48:15 +0300

    solved

---
+++ Pascal/p132_7.pas
@@ -0,0 +1,20 @@
+var
+  i, m, num, prev: integer;
+  state:boolean;
+begin
+  m:=10000;
+  prev:=0;
+  state:=True;
+  write('Enter 4dig number: ');
+  read(num);
+  
+
+  for i := 1 to 4 do begin
+    //writeln(num mod m div (m div 10));
+    if (num mod m div (m div 10)<=prev) then
+      state:=False;
+    prev := num mod m div (m div 10);
+    m:=m div 10;
+  end;
+  write(state);
+end.
