c051e225c966f29744ee4868fb9b0c3d29fb5fe6
MinedroidScr <mineall@mail.ru>
2018-03-02 22:26:32 +0300

    solved

--- Pascal/p143_36.pas
+++ Pascal/p143_36.pas
@@ -1,14 +1,11 @@
 var
-  i,d,n,summ:integer;
+  d,n,summ:integer;
 begin
   read(n);
-  for i:=1 to n do begin
   summ:=0;
-    for d:=1 to i-1 do begin
-      if i mod d =0 then
+    for d:=1 to n-1 do begin
+      if n mod d =0 then
         summ:=summ+d;
     end;
-    if(summ=i)then
-      writeln(i)
-  end;
+    writeln(summ=n)
 end.
